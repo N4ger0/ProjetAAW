ba25f0d60f9e8b854765192d6ba45c3f08a0bf76
Oregan <oregan.hardy@etu.univ-poitiers.fr>
2024-11-14 14:57:14 +0100

    Small css fixes + favicon

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-const express = require("express") ;
+const express = require("express");
 const cookieSession = require('cookie-session')
 require('dotenv').config()
 const axios = require('axios');
+const {expressCspHeader, INLINE, NONE, SELF} = require('express-csp-header');
 
-const port = 3000 ;
-const app = express() ;
+const port = 3000;
+const app = express();
 
 const path = require('path');
 const {redirect} = require("react-router-dom");
@@ -14,14 +15,24 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieSession({
-    keys: [process.env.SESSION_SECRET ],
+    keys: [process.env.SESSION_SECRET],
     secure: false,
     httpOnly: false,
     sameSite: 'lax',
     domain: 'localhost'
 }))
 
-app.get('/', (req,res) => {
+app.use(expressCspHeader({
+    directives: {
+        'default-src': [SELF],
+        'script-src': ["http://localhost:1234", 'unsafe-eval'],
+        'style-src': ['http://localhost:1234'],
+        'img-src': ['*'],
+        'font-src' : ['*']
+    }
+}));
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
@@ -58,6 +69,7 @@ app.get('/api/getUserInfo', async (req, res) => {
             ? `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`
             : `https://cdn.discordapp.com/embed/avatars/${userData.discriminator % 5}.png`; // Fallback URL
 
+        req.session.discord_id = userData.id;
         // Send the global_name and avatar URL in the response
         res.json({
             global_name: userData.global_name,
@@ -70,8 +82,8 @@ app.get('/api/getUserInfo', async (req, res) => {
 });
 
 app.get('/auth/discord/callback', async (req, res) => {
-    console.log(process.env.CLIENT_ID );
-    console.log(process.env.CLIENT_SECRET );
+    console.log(process.env.CLIENT_ID);
+    console.log(process.env.CLIENT_SECRET);
     let code = req.query.code;
 
     const headers = {
@@ -80,16 +92,16 @@ app.get('/auth/discord/callback', async (req, res) => {
 
     const response = await axios.post(
         "https://discord.com/api/oauth2/token",
-        `client_id=${process.env.CLIENT_ID }&client_secret=${process.env.CLIENT_SECRET }&grant_type=authorization_code&code=${code.toString()}&redirect_uri=${process.env.REDIRECT_URI}&scope=identify`,
+        `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code&code=${code.toString()}&redirect_uri=${process.env.REDIRECT_URI}&scope=identify`,
         {
             headers
         }
     );
 
     if (response.data) {
-        const data = response.data ;
+        const data = response.data;
 
-        req.session.access_token = data.access_token ;
+        req.session.access_token = data.access_token;
         req.session.token_type = data.token_type;
         req.session.expires_in = data.expires_in;
         req.session.refresh_token = data.refresh_token;
@@ -102,5 +114,5 @@ app.get('/auth/discord/callback', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`) ;
+    console.log(`Example app listening on port ${port}`);
 })
